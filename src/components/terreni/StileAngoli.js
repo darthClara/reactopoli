c77ec3158fddfd7883cd806b0f860b64fd712c4a
Clara Schillaci <dhdjjk>
2022-11-08 18:57:47 +0100

    fixed sistema selezione pedina (ora funziona)

--- src/components/terreni/StileAngoli.js
+++ src/components/terreni/StileAngoli.js
@@ -3,9 +3,10 @@ import "./StileAngoli.scss"
 import UtenteAutomatico from "../utente/UtenteAutomatico"
 
 export default function StileAngoli(props) {
+    
     return (
         <div className={`stileAngoliWrapper ` + (`stileAngoliWrapper${props.tema}`)}>
-            <UtenteAutomatico/>
+            <UtenteAutomatico nome={props.nome} immagine={props.pedina} id={props.id} posizione={1}/>
             {props.testo && <h2>{props.testo}</h2>}
         </div>
     )
